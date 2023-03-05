70ac7b0b9968d7a46e2f7f8041ae3420b8972d92
sagarpadwekar <imsagarpadwekar@gmail.com>
2023-03-05 20:05:25 +0530

    changes made by sagar

---
+++ src/main/java/com/BikkadIT/gitDemo/App.java
@@ -0,0 +1,17 @@
+package com.BikkadIT.gitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    static {
+    	System.out.println("program executed");
+    }
+}
